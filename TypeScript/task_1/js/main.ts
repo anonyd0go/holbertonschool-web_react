301f647b307fcfc4fb777ee0e55f97c9f2096bb3
DDF <133790676+anonyd0go@users.noreply.github.com>
2025-05-26 17:38:11 -0400

    Update main.ts task_1 for exercise 3

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -7,7 +7,19 @@ interface Teacher {
     location: string;
     [propName: string]: unknown;
 }
-
+// Directors extends Teacher
 interface Directors extends Teacher {
     numberOfReports: number;
+}
+
+// TODO: confirm if this is what they ment when
+// talking about function interfaces.
+// Interface for printTeacher function
+interface printTeacherFunction {
+    (firstName:string, lastName: string): void;
+}
+// Function
+let printTeacher: printTeacherFunction;
+printTeacher = (firstName:string, lastName: string): void => {
+    console.log(`${firstName.charAt(0)}. ${lastName}`);
 }
