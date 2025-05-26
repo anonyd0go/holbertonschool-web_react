0fe644f6329d5cd4268385813c89969bdf41ea5f
DDF <133790676+anonyd0go@users.noreply.github.com>
2025-05-26 17:08:01 -0400

    Add initial main.ts of task_1

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -7,14 +7,3 @@ interface Teacher {
     location: string;
     [propName: string]: unknown;
 }
-
-
-const teacher3: Teacher = {
-  firstName: 'John',
-  fullTimeEmployee: false,
-  lastName: 'Doe',
-  location: 'London',
-  contract: false,
-};
-
-console.log(teacher3);
