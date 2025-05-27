008abbc77d9b5454e251938c2225471525b33c3b
DDF <133790676+anonyd0go@users.noreply.github.com>
2025-05-27 16:13:51 -0400

    Update main.ts file for task_1

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -1,25 +1,69 @@
-// Create an interface for Teacher
-interface Teacher {
-    readonly firstName: string;
-    readonly lastName: string;
-    fullTimeEmployee: boolean;
-    yearsOfExperience?: number;
-    location: string;
-    [propName: string]: unknown;
-}
-// Directors extends Teacher
-interface Directors extends Teacher {
-    numberOfReports: number;
+/**
+ * @module main
+ * @description Main module implementing the interfaces for teacher and student management
+ */
+
+import { Teacher, Directors, studentConstructor } from "./interfaces";
+import { printTeacherFunction } from "./interfaces";
+import { studentClass } from "./interfaces";
+
+/**
+ * @function printTeacher
+ * @description Formats teacher's name as first initial followed by full last name
+ * @param {string} firstName - Teacher's first name
+ * @param {string} lastName - Teacher's last name
+ * @returns {string} Formatted string in the pattern "F. LastName"
+ */
+const printTeacher: printTeacherFunction = (firstName, lastName) => {
+    return `${firstName.charAt(0)}. ${lastName}`;
 }
 
-// TODO: confirm if this is what they ment when
-// talking about function interfaces.
-// Interface for printTeacher function
-interface printTeacherFunction {
-    (firstName:string, lastName: string): void;
+/**
+ * @class StudentClass
+ * @implements {studentClass}
+ * @description Implementation of the studentClass interface
+ */
+class StudentClass implements studentClass {
+    firstName: string;
+    lastName: string;
+
+    /**
+     * @constructor
+     * @param {string} firstName - Student's first name
+     * @param {string} lastName - Student's last name
+     */
+    constructor(firstName:string, lastName:string) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    /**
+     * @method workOnHomework
+     * @description Indicates the current work status of the student
+     * @returns {string} Status message indicating work in progress
+     */
+    workOnHomework() {
+        return 'Currently working';
+    }
+
+    /**
+     * @method displayName
+     * @description Retrieves the student's first name
+     * @returns {string} Student's first name
+     */
+    displayName() {
+        return this.firstName;
+    }
 }
-// Function
-let printTeacher: printTeacherFunction;
-printTeacher = (firstName:string, lastName: string): void => {
-    console.log(`${firstName.charAt(0)}. ${lastName}`);
+
+/**
+ * @function createClass
+ * @description Factory function to create new student instances
+ * @param {studentConstructor} ctor - Constructor for creating student instances
+ * @param {string} firstName - Student's first name
+ * @param {string} lastName - Student's last name
+ * @returns {studentClass} New student instance
+ */
+function createClass(ctor: studentConstructor, firstName: string, lastName: string) {
+    return new ctor(firstName, lastName);
 }
