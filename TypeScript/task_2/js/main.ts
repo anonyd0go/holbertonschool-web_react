34a6c29d1491a143b7821f99fd07d4fef11861b4
DDF <133790676+anonyd0go@users.noreply.github.com>
2025-05-27 17:02:28 -0400

    Add main.ts with exercise 5

---
+++ TypeScript/task_2/js/main.ts
@@ -0,0 +1,99 @@
+import { DirectorInterface, TeacherInterface } from "./interfaces";
+import { EmployeeConstructor } from "./interfaces";
+
+/**
+ * Director class implementing DirectorInterface
+ * @class Director
+ * @implements {DirectorInterface}
+ */
+class Director implements DirectorInterface {
+    /** Director's salary which can be a number or string */
+    salary: number | string;
+
+    /**
+     * Creates an instance of Director
+     * @param {(number|string)} salary - The director's salary
+     */
+    constructor(salary: number | string) {
+        this.salary = salary;
+    }
+
+    /**
+     * Determines if director can work from home
+     * @returns {string} Message indicating work from home status
+     */
+    workFromHome() {
+        return 'Working from home';
+    }
+
+    /**
+     * Determines coffee break eligibility
+     * @returns {string} Message about coffee break status
+     */
+    getCoffeeBreak() {
+        return 'Getting a coffee break';
+    }
+
+    /**
+     * Handles director tasks
+     * @returns {string} Message about director tasks
+     */
+    workDirectorTasks() {
+        return 'Getting to director tasks';
+    }
+}
+
+/**
+ * Teacher class implementing TeacherInterface
+ * @class Teacher
+ * @implements {TeacherInterface}
+ */
+class Teacher implements TeacherInterface {
+    /** Teacher's salary which can be a number or string */
+    salary: number | string;
+
+    /**
+     * Creates an instance of Teacher
+     * @param {(number|string)} salary - The teacher's salary
+     */
+    constructor(salary: number | string) {
+        this.salary = salary;
+    }
+
+    /**
+     * Determines if teacher can work from home
+     * @returns {string} Message indicating work from home status
+     */
+    workFromHome() {
+        return 'Cannot work from home';
+    }
+
+    /**
+     * Determines coffee break eligibility
+     * @returns {string} Message about coffee break status
+     */
+    getCoffeeBreak() {
+        return 'Cannot have a break';
+    }
+
+    /**
+     * Handles teacher tasks
+     * @returns {string} Message about teacher tasks
+     */
+    workTeacherTasks() {
+        return 'Getting to work';
+    }
+}
+
+/**
+ * Factory function to create either a Teacher or Director based on salary
+ * @type {EmployeeConstructor}
+ * @param {(number|string)} salary - The employee's salary
+ * @returns {(Director|Teacher)} An instance of either Director or Teacher
+ */
+const createEmployee: EmployeeConstructor = (salary: number | string): Director | Teacher => {
+    if (typeof salary === 'number' && salary < 500) {
+        return new Teacher(salary);
+    }
+    return new Director(salary);
+}
