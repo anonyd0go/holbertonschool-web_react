b610b540c1dec0942ec79c5a6d94fbb95c7346f8
DDF <133790676+anonyd0go@users.noreply.github.com>
2025-05-27 16:04:24 -0400

    Add interfaces.js file for task_1

---
+++ TypeScript/task_1/js/interfaces.ts
@@ -0,0 +1,55 @@
+/**
+ * @module interfaces
+ * This module contains all interfaces for task_1 exercises
+ */
+
+/**
+ * @interface Teacher
+ * @description Represents a teacher with basic information and dynamic properties
+ * @property {string} firstName - The teacher's first name (read-only)
+ * @property {string} lastName - The teacher's last name (read-only)
+ * @property {boolean} fullTimeEmployee - Indicates if the teacher is full-time
+ * @property {number} [yearsOfExperience] - Optional years of teaching experience
+ * @property {string} location - The teacher's work location
+ * @property {unknown} [propName] - Additional dynamic properties
+ */
+export interface Teacher {
+    readonly firstName: string;
+    readonly lastName: string;
+    fullTimeEmployee: boolean;
+    yearsOfExperience?: number;
+    location: string;
+    [propName: string]: unknown;
+}
+
+/**
+ * @interface Directors
+ * @extends {Teacher}
+ * @description Represents a director, extending the Teacher interface
+ * @property {number} numberOfReports - The number of reports managed by the director
+ */
+export interface Directors extends Teacher {
+    numberOfReports: number;
+}
+
+/**
+ * @interface printTeacherFunction
+ * @description Function type for printing teacher information
+ * @param {string} firstName - The teacher's first name
+ * @param {string} lastName - The teacher's last name
+ * @returns {string} Formatted string representation of the teacher
+ */
+export interface printTeacherFunction {
+    (firstName:string, lastName: string): string;
+}
+
+export interface studentConstructor {
+    new ( firstName: string, lastName:string ): studentClass;
+}
+
+export interface studentClass {
+    firstName: string;
+    lastName: string;
+    workOnHomework(): string;
+    displayName(): string;
+}
