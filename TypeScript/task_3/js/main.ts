225925b7ac599114b70f637fe0a040870fd1b000
DDF <133790676+anonyd0go@users.noreply.github.com>
2025-05-28 17:29:21 -0400

    Update comments in main.ts

--- TypeScript/task_3/js/main.ts
+++ TypeScript/task_3/js/main.ts
@@ -2,19 +2,22 @@
 import { RowElement, RowID } from "./interface";
 import * as CRUD from "./crud";
 
+// Create new RowElement Interface object
 let row: RowElement = {
     firstName: 'Guillaume',
     lastName: 'Salva',
 }
-
+// Get row id for new row element
 const newRowID: RowID = CRUD.insertRow(row);
 
+// Create an updated row element
 const updatedRow: RowElement = {
     firstName: 'Guillaume',
     lastName: 'Salva',
     age: 23,
 };
 
+// Update existing row
 CRUD.updateRow(newRowID, updatedRow);
 
 /* Don't know if I need this line but was in task
