37c4152a3f8f04a2d4acf9731703f4f2dfd6b116
DDF <133790676+anonyd0go@users.noreply.github.com>
2025-05-28 17:39:52 -0400

    Add Teachers.ts to task_4

---
+++ TypeScript/task_4/js/subjects/Teacher.ts
@@ -0,0 +1,15 @@
+/**
+ * Namespace containing subject-related interfaces and implementations
+ */
+namespace Subjects {
+    /**
+     * Interface representing a teacher
+     * @interface Teacher
+     * @property {string} firstName - The teacher's first name
+     * @property {string} lastName - The teacher's last name
+     */
+    export interface Teacher {
+        firstName: string;
+        lastName: string;
+    }
+}
